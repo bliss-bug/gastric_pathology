aa2112da704ef8a8ec2ce587f1aa34fd7f1d9b46
bliss-bug <191220184@smail.nju.edu.cn>
2024-12-20 19:22:56 +0800

    update train

--- train_gigapath.py
+++ train_gigapath.py
@@ -136,7 +136,7 @@ if __name__=='__main__':
     
     parser.add_argument('--seed', default=123, type=int)
     parser.add_argument('--fold', default=1, type=int)
-    parser.add_argument('--data_path', nargs='+', type=str, default=['WSI/features/gigapath_features', 'WSI/features2/gigapath_features'])
+    parser.add_argument('--data_path', nargs='+', type=str, default=['WSI/features/gigapath_features', 'WSI/features2/gigapath_features', 'WSI/features3/gigapath_features'])
     parser.add_argument('--label_path', default='labels/NDPI_labels.xlsx', type=str)
     parser.add_argument('--device', default='cuda:1', type=str)
     parser.add_argument('--pretrained', default='checkpoints/slide_encoder.pth', type=str)
--- train_shell.py
+++ train_shell.py
@@ -1,8 +1,8 @@
 import os
 
 extractions = ['gigapath', 'uni', 'resnet50']
-#models = ['abmil', 'dsmil', 'transmil', 'rrtmil']
-models = ['clam_sb', 'clam_mb']
+models = ['abmil', 'dsmil', 'clam_sb', 'clam_mb', 'transmil', 'rrtmil']
+#models = []
 
 size = {'gigapath': 1536, 'uni': 1024, 'resnet50': 1024}
 device = 'cuda:0'
@@ -12,16 +12,17 @@ for model in models:
     for extraction in extractions:
         for i in range(1, 6):
             cmd = f'python train.py --feat_size={size[extraction]} --data_path \'WSI/features/{extraction}_features\' \
-                \'WSI/features2/{extraction}_features\' --model=\'{model}\' --fold={i} --device={device} --epochs={epochs}'
+                \'WSI/features2/{extraction}_features\' \'WSI/features3/{extraction}_features\' \
+                --model=\'{model}\' --fold={i} --device={device} --epochs={epochs}'
             os.system(cmd)
 
-'''
+
 for extraction in extractions:
     for i in range(1, 6):
         cmd = f'python train_DTFD.py --feat_size={size[extraction]} --data_path \'WSI/features/{extraction}_features\' \
-            \'WSI/features2/{extraction}_features\' --fold={i} --device={device} --epochs={epochs}'
+            \'WSI/features2/{extraction}_features\' \'WSI/features2/{extraction}_features\' \
+            --fold={i} --device={device} --epochs={epochs}'
         os.system(cmd)
-'''
 
 '''
 for i in range(1, 6):
