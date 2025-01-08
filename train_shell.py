46bec93bfaffa966e48a8a75dc635b711dccdaef
bliss-bug <191220184@smail.nju.edu.cn>
2025-01-08 13:33:44 +0800

    update train_shell

--- train_shell.py
+++ train_shell.py
@@ -7,8 +7,9 @@ parser.add_argument('--device', default='cuda:2', type=str)
 args = parser.parse_args()
 
 extractions = ['gigapath', 'uni', 'resnet50']
-models = ['abmil', 'dsmil', 'clam_sb', 'clam_mb', 'transmil', 'rrtmil']
-models = ['longmil']
+models = ['abmil', 'dsmil', 'clam_sb', 'clam_mb']
+models = ['transmil', 'rrtmil', 'longmil']
+models = ['lbmil']
 
 size = {'gigapath': 1536, 'uni': 1024, 'resnet50': 1024}
 device = args.device
@@ -22,14 +23,13 @@ for model in models:
                 --model=\'{model}\' --fold={i} --device={device} --epochs={epochs}'
             os.system(cmd)
 
-'''
+
 for extraction in extractions:
     for i in range(1, 6):
         cmd = f'python train_DTFD.py --feat_size={size[extraction]} --data_path \'WSI/features/{extraction}_features\' \
-            \'WSI/features2/{extraction}_features\' \'WSI/features2/{extraction}_features\' \
+            \'WSI/features2/{extraction}_features\' \'WSI/features3/{extraction}_features\' \
             --fold={i} --device={device} --epochs={epochs}'
         os.system(cmd)
-'''
         
 '''
 for i in range(1, 6):
